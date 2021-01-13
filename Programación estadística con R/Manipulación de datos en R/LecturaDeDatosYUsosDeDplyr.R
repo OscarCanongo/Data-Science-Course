8408876fab8ee1003e6a6227c204f4ffca1c071a
OscarCanongo <oscarcanongo@outlook.com>
2021-01-12 22:20:22 -0600

    Manipulación de datos en R

---
+++ Programación estadística con R/Manipulación de datos en R/LecturaDeDatosYUsosDeDplyr.R
@@ -0,0 +1,7 @@
+Bundesliga2021 <- read.csv("~/Desktop/Data-Science-Course/Programación estadística con R/Introducción a R y Software/data/Bundesliga2020-2021.csv")
+Bundesliga2020 <- read.csv("~/Desktop/Data-Science-Course/Programación estadística con R/Introducción a R y Software/data/Bundesliga2019-2020.csv")
+Bundesliga2019 <- read.csv("~/Desktop/Data-Science-Course/Programación estadística con R/Introducción a R y Software/data/Bundesliga2018-2019.csv")
+Bundesliga2018 <- read.csv("~/Desktop/Data-Science-Course/Programación estadística con R/Introducción a R y Software/data/Bundesliga2017-2018.csv")
+
+install.packages("dplyr")
+library(dplyr)
