4d3e766cd346e6b45a69d9f359b7cbe909ad82cc
OscarCanongo <oscarcanongo@outlook.com>
2021-01-29 10:12:24 -0600

    Columna sumagoles

---
+++ Programación estadística con R/Series de tiempo/Postwork6.R
@@ -0,0 +1,6 @@
+#Importar el csv
+matches <- read.csv("~/Desktop/Data-Science-Course/Programación estadística con R/Series de tiempo/Data/match.csv")
+
+#Agrega una nueva columna sumagoles que contenga la suma de goles por partido.
+library(dplyr)
+matches <- mutate(matches, sumagoles = home.score + away.score)
