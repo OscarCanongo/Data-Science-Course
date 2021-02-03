756c02fbef70b6881a5ab5dd8855199e776c4eed
OscarCanongo <oscarcanongo@outlook.com>
2021-02-03 10:17:09 -0600

    Postwork 7

--- Programación estadística con R/RStudio Cloud - Github, conexiones con BDs y lectura de datos externos/Postwork7.R
+++ Programación estadística con R/RStudio Cloud - Github, conexiones con BDs y lectura de datos externos/Postwork7.R
@@ -1,4 +1,26 @@
 #Alojar el fichero data.csv en una base de datos llamada match_games, nombrando al collection como match
-
 install.packages("mongolite")
 library(mongolite)
+
+match=data.table::fread("C:/Users/end user/OneDrive/Escritorio/DataScience/Sesion7/Data/data.csv")
+
+#Conexión
+conexion <- mongo(
+  collection = "match",
+  db = "match_games",
+  url = "mongodb+srv://admin:oscar@cluster0.laeuo.mongodb.net/bedu?retryWrites=true&w=majority",
+  verbose = FALSE,
+  options = ssl_options()
+)
+
+#Agregando información 
+conexion$insert(match)
+
+#Una vez hecho esto, realizar un count para conocer el número de registros que se tiene en la base
+conexion$count()
+
+#Realiza una consulta utilizando la sintaxis de Mongodb, en la base de datos para conocer el número de goles que metió el Real Madrid el 20 de diciembre de 2015 y contra que equipo jugó, ¿perdió ó fue goleada?
+conexion$find('{"Date":"2015-12-20","HomeTeam":"Real Madrid"}')
+
+# Cerrando la conexión
+rm(my_collection)
